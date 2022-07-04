2c11b688cd48c0e115520cea75962bf769d43786
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-04 16:32:26 -0400

    Atualizações

--- src/os/status/index.php
+++ src/os/status/index.php
@@ -5,59 +5,64 @@
     $result = mysqli_query($con, $query);
 
 ?>
-<div class="col">
-    <div class="col d-flex justify-content-between">
-        <div class="p-2"><h5>Status da OS</h5></div>
-        <div class="p-2">
-            <button
-                class="btn btn-primary"
-                data-bs-toggle="offcanvas"
-                href="#offcanvasDireita"
-                role="button"
-                aria-controls="offcanvasDireita"
-                offcanvasDireita
-            >
-                <i class="fa-solid fa-plus"></i>
-                Novo
-            </button>
-        </div>
-    </div>
-</div>
-<table id="TableStatus" class="table table-hover" style="width:100%">
-    <thead>
-        <tr>
-            <th>Título</th>
-            <th>Situação</th>
-            <!-- <th>Ações</th> -->
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        while($d = mysqli_fetch_object($result)){
-        ?>
-        <tr linha='<?=$d->codigo?>'>
-            <td><?=$d->titulo?></td>
-            <td><?=$d->situacao?></td>
-            <!-- <td>
+<div class="row p-3">
+    <div class="col">
+        <div class="col d-flex justify-content-between">
+            <div class="p-2"><h5>Status da OS</h5></div>
+            <div class="p-2">
                 <button
-                    editar="<?=$d->codigo?>"
-                    class="btn btn-success btn-xs"
+                    class="btn btn-primary"
                     data-bs-toggle="offcanvas"
                     href="#offcanvasDireita"
                     role="button"
                     aria-controls="offcanvasDireita"
+                    offcanvasDireita
                 >
-                    Ed
+                    <i class="fa-solid fa-plus"></i>
+                    Novo
                 </button>
-                <button excluir="<?=$codigo?>" class="btn btn-danger btn-xs">ex</button>
-            </td> -->
-        </tr>
-        <?php
-        }
-        ?>
-    </tbody>
-</table>
-
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row p-3">
+    <div class="col">
+        <table id="TableStatus" class="table table-hover" style="width:100%">
+            <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Situação</th>
+                    <!-- <th>Ações</th> -->
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                while($d = mysqli_fetch_object($result)){
+                ?>
+                <tr linha='<?=$d->codigo?>'>
+                    <td><?=$d->titulo?></td>
+                    <td><?=$d->situacao?></td>
+                    <!-- <td>
+                        <button
+                            editar="<?=$d->codigo?>"
+                            class="btn btn-success btn-xs"
+                            data-bs-toggle="offcanvas"
+                            href="#offcanvasDireita"
+                            role="button"
+                            aria-controls="offcanvasDireita"
+                        >
+                            Ed
+                        </button>
+                        <button excluir="<?=$codigo?>" class="btn btn-danger btn-xs">ex</button>
+                    </td> -->
+                </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
 <script>
     $(document).ready(function () {
 
