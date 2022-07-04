1100a99687aeddd20715a4cb2495b742655fe2ca
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-04 16:48:25 -0400

    Atualizações

---
+++ src/os/classificacao/form.php
@@ -0,0 +1,132 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkos/lib/includes.php");
+
+    if($_POST['acao'] == 'salvar'){
+
+        $data = $_POST;
+        $attr = [];
+
+        unset($data['codigo']);
+        unset($data['acao']);
+
+        foreach ($data as $name => $value) {
+            $attr[] = "{$name} = '" . mysqli_real_escape_string($con, $value) . "'";
+        }
+        $attr = implode(', ', $attr);
+
+        if($_POST['codigo']){
+            $query = "update os_classificacao set {$attr} where codigo = '{$_POST['codigo']}'";
+            mysqli_query($con, $query);
+            $cod = $_POST['codigo'];
+        }else{
+            $query = "insert into os_classificacao set data_cadastro = NOW(), deletado = '{\"usuario\":\"\", \"data\":\"\"}', {$attr}";
+            mysqli_query($con, $query);
+            $cod = mysqli_insert_id($con);
+        }
+
+        $retorno = [
+            'status' => true,
+            'codigo' => $cod
+        ];
+
+        echo json_encode($retorno);
+
+        exit();
+    }
+
+
+    if($_POST['status']){
+        $query = "select * from os_classificacao where codigo = '{$_POST['status']}'";
+        $result = mysqli_query($con, $query);
+        $d = mysqli_fetch_object($result);
+    }
+
+
+?>
+<style>
+    .Topo<?=$md5?> {
+        position:absolute;
+        left:60px;
+        top:8px;
+        z-index:0;
+    }
+</style>
+<h4 class="Topo<?=$md5?>">Dados do Status</h4>
+<div class="row">
+    <div class="col">
+        <form id="form-<?= $md5 ?>">
+        <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título" value="<?=$d->titulo?>">
+                <label for="titulo">Título</label>
+            </div>
+
+            <div class="form-floating mb-3">
+                <select class="form-select" id="situacao" name="situacao" aria-label="Situação">
+                    <option value="1" <?=(($d->situacao == '1')?'selected':false)?> >Liberado</option>
+                    <option value="0" <?=(($d->situacao == '0')?'selected':false)?>>Bloqueado</option>
+                </select>
+                <label for="situacao">Situação</label>
+            </div>
+            <input type="hidden" name="codigo" id="codigo" value="<?=$d->codigo?>">
+            <button
+                salvar
+                class="btn btn-primary"
+                type="submit"
+                data-bs-toggle="offcanvas"
+                href="#offcanvasDireita"
+                role="button"
+                aria-controls="offcanvasDireita"
+            >
+                Salvar
+            </button>
+            <button
+                cancelar
+                class="btn btn-danger"
+                type="button"
+                data-bs-toggle="offcanvas"
+                href="#offcanvasDireita"
+                role="button"
+                aria-controls="offcanvasDireita"
+            >
+                Cancelar
+            </button>
+        </form>
+    </div>
+</div>
+
+<script>
+    $(function(){
+
+        $('#form-<?=$md5?>').submit(function (e) {
+            e.preventDefault();
+
+            var codigo = $('#codigo').val();
+            var campos = $(this).serializeArray();
+
+            if (codigo) {
+                campos.push({name: 'codigo', value: codigo})
+            }
+
+            campos.push({name: 'acao', value: 'salvar'})
+            $.ajax({
+                url: 'src/os/classificacao/form.php',
+                type:"POST",
+                dataType:"json",
+                data: campos,
+                success: function (dados) {
+                    empresa = dados.codigo;
+                    $.ajax({
+                        url:"src/os/classificacao/index.php",
+                        type:"POST",
+                        success:function(dados){
+                            $("#paginaHome").html(dados);
+                        }
+                    });
+
+                }
+            })
+
+
+        });
+    })
+</script>
---
+++ src/os/classificacao/index.php
@@ -0,0 +1,104 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkos/lib/includes.php");
+
+    $query = "select *, if(situacao = '1', 'Liberado', 'Bloqueado') as situacao from os_classificacao order by titulo";
+    $result = mysqli_query($con, $query);
+
+?>
+
+<div class="col-12">
+    <div class="col d-flex justify-content-between">
+        <div class="p-3"><h5>Classificação da OS</h5></div>
+        <div class="p-3">
+            <button
+                class="btn btn-primary"
+                data-bs-toggle="offcanvas"
+                href="#offcanvasDireita"
+                role="button"
+                aria-controls="offcanvasDireita"
+                offcanvasDireita
+            >
+                <i class="fa-solid fa-plus"></i>
+                Novo
+            </button>
+        </div>
+    </div>
+</div>
+
+<div class="col-12">
+    <div class="p-3">
+        <table id="TableStatus" class="table table-hover" style="width:100%">
+            <thead>
+                <tr>
+                    <th>Título</th>
+                    <th>Situação</th>
+                    <!-- <th>Ações</th> -->
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                while($d = mysqli_fetch_object($result)){
+                ?>
+                <tr linha='<?=$d->codigo?>'>
+                    <td><?=$d->titulo?></td>
+                    <td><?=$d->situacao?></td>
+                    <!-- <td>
+                        <button
+                            editar="<?=$d->codigo?>"
+                            class="btn btn-success btn-xs"
+                            data-bs-toggle="offcanvas"
+                            href="#offcanvasDireita"
+                            role="button"
+                            aria-controls="offcanvasDireita"
+                        >
+                            Ed
+                        </button>
+                        <button excluir="<?=$codigo?>" class="btn btn-danger btn-xs">ex</button>
+                    </td> -->
+                </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+</div>
+
+<script>
+    $(document).ready(function () {
+
+        $("button[offcanvasDireita]").click(function(){
+            $.ajax({
+                url:"src/os/classificacao/form.php",
+                success:function(dados){
+                    $(".LateralDireita").html(dados);
+                }
+            });
+        });
+
+
+        $("tr[linha]").click(function(){
+            status = $(this).attr("linha");
+
+            $.ajax({
+                url:"src/os/classificacao/form.php",
+                type:"POST",
+                data:{
+                    status,
+                },
+                success:function(dados){
+                    $(".LateralDireita").html(dados);
+
+                    let myOffCanvas = document.getElementById('offcanvasDireita');
+                    let openedCanvas = bootstrap.Offcanvas.getInstance(myOffCanvas);
+                    openedCanvas.show();
+
+                }
+            });
+        });
+
+
+
+    });
+</script>
