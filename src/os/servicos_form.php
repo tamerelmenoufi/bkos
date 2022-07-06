543112d2cf2d5145973f0f0e535655a1b8664121
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-06 15:45:47 -0400

    Atualizações

--- src/os/servicos_form.php
+++ src/os/servicos_form.php
@@ -59,17 +59,17 @@
     <div class="col">
         <form id="form-<?= $md5 ?>">
             <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título" value="<?=$d->titulo?>">
+                <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título" value="<?=$d->titulo?>" required>
                 <label for="titulo">Título</label>
             </div>
             <div class="form-floating mb-3">
-                <textarea name="descricao" id="descricao" class="form-control" style="height:120px;" placeholder="Descrição"><?=$d->descricao?></textarea>
+                <textarea name="descricao" id="descricao" class="form-control" style="height:120px;" placeholder="Descrição" required><?=$d->descricao?></textarea>
                 <label for="descricao">Descricão</label>
             </div>
 
 
             <div class="form-floating mb-3">
-                <select class="form-select" name="empresa_endereco" id="empresa_endereco">
+                <select class="form-select" name="empresa_endereco" id="empresa_endereco" required>
                     <option value="">::Selecione::</option>
                     <?php
                     $q = "select * from empresas_enderecos where situacao = '1' and empresa = '{$_SESSION['empresa']}' order by nome";
@@ -86,7 +86,7 @@
 
 
             <div class="form-floating mb-3">
-                <select class="form-select" name="executor" id="executor">
+                <select class="form-select" name="executor" id="executor" required>
                     <option value="">::Selecione::</option>
                     <?php
                     $q = "select * from colaboradores where situacao = '1' order by nome";
