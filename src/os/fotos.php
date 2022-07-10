00d515e101789c44f2f3943b0ddc08930b7ec919
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-10 12:12:29 -0400

    Atualizações

--- src/os/fotos.php
+++ src/os/fotos.php
@@ -115,7 +115,7 @@
 <h4 class="Topo<?=$md5?>">Lista de fotos da OS #<?=str_pad($_POST['os'] , 6 , '0' , STR_PAD_LEFT)?></h4>
 <div class="row">
     <div class="col">
-        <div class="card">
+        <div class="card m-3">
             <h5><?=$d->titulo?></h5>
             <p><?=$d->descrição?></p>
         </div>
