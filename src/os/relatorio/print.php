94caab9e178c4aaebdb02c4c7142286b52d0035b
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-11 11:37:40 -0400

    Atualizações

--- src/os/relatorio/print.php
+++ src/os/relatorio/print.php
@@ -171,8 +171,38 @@ $html = '<!DOCTYPE html>
         $i++;
     }
 
-    $html .= '</div>
-</body>
+    $html .= '</div>';
+
+
+    $html .= '<div style="page-break-before: always;"></div>
+    <div class="corpo">';
+
+    $q = "select * from os_fotos where cod_os = '{$d->codigo}'";
+    $r = mysqli_query($con, $q);
+    $i=0;
+    while($e = mysqli_fetch_object($r)){
+        if($i%2 == 0){
+            $html .= '<div class="corpo"></div>';
+            // $html .= '<h2 class="titulo">REGISTRO FOTOGRÁFICO</h2>';
+        }
+        if($i%6 == 0 and $i > 0){
+            $html .= '<div style="page-break-before: always;"></div>';
+            $html .= '<h2 class="titulo">REGISTRO FOTOGRÁFICO</h2>';
+        }else if($i == 0){
+            $html .= '<h2 class="titulo">REGISTRO FOTOGRÁFICO</h2>';
+        }
+
+        $html .= '<div class="divImg">
+                    <div class="img" style="background-image:url(http://os.bkmanaus.com.br/src/os/fotos/'.$d->codigo.'/'.$e->foto.')"></div>
+                    <div class="titulo_imagem">'.$e->titulo.'</div>
+                  </div>';
+        $i++;
+    }
+
+    $html .= '</div>';
+
+
+$html .= '</body>
 </html>';
 
 
