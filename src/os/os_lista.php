0391bb13919577bf7f252a7d42ab59dd473afa8b
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-18 11:56:03 -0400

    Atualizações

--- src/os/os_lista.php
+++ src/os/os_lista.php
@@ -49,28 +49,9 @@
 
     </div>
 </div> -->
-<div style="width:100%">
-<div class="row">
-    <div class="col">
-        <div class="col d-flex justify-content-between">
-            <div class="p-2"><h5>Ordem de Serviços</h5></div>
-            <!-- <div class="p-2">
-                <button
-                    class="btn btn-primary"
-                    data-bs-toggle="offcanvas"
-                    href="#offcanvasDireita"
-                    role="button"
-                    aria-controls="offcanvasDireita"
-                    offcanvasDireita
-                >
-                    <i class="fa-solid fa-plus"></i>
-                    Novo
-                </button>
-            </div> -->
-        </div>
-    </div>
-</div>
-</div>
+
+
+<h5>Ordem de Serviços</h5>
 
 
     <div class="col">
