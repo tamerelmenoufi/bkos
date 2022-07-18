a41454c6aac81a7a8ad3d9f9d5494ef9f2f7dcf8
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-18 11:58:39 -0400

    Atualizações

--- src/os/os_lista.php
+++ src/os/os_lista.php
@@ -70,29 +70,29 @@
         while($d = mysqli_fetch_object($result)){
         ?>
         <div class="p-2 tb-b">
-        <div class="row">
-            <div class="col-md-2">#<?=str_pad($d->codigo , 6 , '0' , STR_PAD_LEFT)?></div>
-            <div class="col-md-2"><?=$d->titulo?></div>
-            <div class="col-md-2"><?=$d->data_cadastro?></div>
-            <div class="col-md-2"><?=$d->situacao?></div>
-            <div class="col-md-2">
+            <div class="row">
+                <div class="col-md-2">#<?=str_pad($d->codigo , 6 , '0' , STR_PAD_LEFT)?></div>
+                <div class="col-md-2"><?=$d->titulo?></div>
+                <div class="col-md-2"><?=$d->data_cadastro?></div>
+                <div class="col-md-2"><?=$d->situacao?></div>
+                <div class="col-md-2">
 
 
-                <div class="dropdown">
-                    <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="acoesOs" data-bs-toggle="dropdown" aria-expanded="false">
-                        Ações
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="acoesOs">
-                        <!-- <li os="<?=$d->codigo?>" url="src/os/servicos_form.php"><a class="dropdown-item" href="#">Editar</a></li> -->
-                        <li os='<?=$d->codigo?>' url="src/os/fotos.php"><a class="dropdown-item" href="#">Registro Fotográfico</a></li>
-                        <li os='<?=$d->codigo?>' url="src/os/eventos.php"><a class="dropdown-item" href="#">Registro de Eventos</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="src/os/relatorio/print.php?os=<?=$d->codigo?>" target="_blank">Relatório</a></li>
-                    </ul>
+                    <div class="dropdown">
+                        <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="acoesOs" data-bs-toggle="dropdown" aria-expanded="false">
+                            Ações
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="acoesOs">
+                            <!-- <li os="<?=$d->codigo?>" url="src/os/servicos_form.php"><a class="dropdown-item" href="#">Editar</a></li> -->
+                            <li os='<?=$d->codigo?>' url="src/os/fotos.php"><a class="dropdown-item" href="#">Registro Fotográfico</a></li>
+                            <li os='<?=$d->codigo?>' url="src/os/eventos.php"><a class="dropdown-item" href="#">Registro de Eventos</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="src/os/relatorio/print.php?os=<?=$d->codigo?>" target="_blank">Relatório</a></li>
+                        </ul>
+                    </div>
+
                 </div>
-
             </div>
-        </div>
         </div>
         <?php
         }
