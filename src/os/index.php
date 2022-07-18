30b11a4c32eae5b05a06e51576eb198d37f98f8d
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-18 09:27:27 -0400

    Atualizações

--- src/os/index.php
+++ src/os/index.php
@@ -31,7 +31,23 @@
         </div>
     </div>
 </div>
-<table id="TableColaboradores" class="table table-hover" style="width:100%">
+
+<div class="col">
+
+
+    <div class="row">
+        <div class="col-md-2">N° Solicitação</div>
+        <div class="col-md-2">Título</div>
+        <div class="col-md-2">Empresa</div>
+        <div class="col-md-2">O.S. Vinculadas</div>
+        <div class="col-md-2">Situação</div>
+        <div class="col-md-2">Ações</div>
+    </div>
+
+
+
+
+<!-- <table id="TableColaboradores" class="table table-hover" style="width:100%">
     <thead>
         <tr>
             <th>N° Solicitação</th>
@@ -42,11 +58,37 @@
             <th>Ações</th>
         </tr>
     </thead>
-    <tbody>
+    <tbody> -->
         <?php
         while($d = mysqli_fetch_object($result)){
         ?>
-        <tr>
+
+
+    <div class="row">
+        <div class="col-md-2"><?=str_pad($d->codigo , 6 , '0' , STR_PAD_LEFT)?></div>
+        <div class="col-md-2"><?=$d->titulo?></div>
+        <div class="col-md-2"><?=$d->nome_empresa?></div>
+        <div class="col-md-2"><?=$d->quantidade?> OS</div>
+        <div class="col-md-2"><?=$d->situacao?></div>
+        <div class="col-md-2">
+
+            <div class="dropdown">
+                <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="acoesOs" data-bs-toggle="dropdown" aria-expanded="false">
+                    Ações
+                </button>
+                <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="acoesOs">
+                    <li linha='<?=$d->codigo?>'><a class="dropdown-item" href="#">Editar</a></li>
+                    <li servico='<?=$d->codigo?>'><a class="dropdown-item" href="#">Ordem de Serviços</a></li>
+                    <!-- <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item" href="#">Separated link</a></li> -->
+                </ul>
+            </div>
+
+        </div>
+    </div>
+
+        <!-- <tr>
             <td><?=str_pad($d->codigo , 6 , '0' , STR_PAD_LEFT)?></td>
             <td><?=$d->titulo?></td>
             <td><?=$d->nome_empresa?></td>
@@ -61,13 +103,13 @@
                     <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="acoesOs">
                         <li linha='<?=$d->codigo?>'><a class="dropdown-item" href="#">Editar</a></li>
                         <li servico='<?=$d->codigo?>'><a class="dropdown-item" href="#">Ordem de Serviços</a></li>
-                        <!-- <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
                         <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Separated link</a></li> -->
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
                     </ul>
                 </div>
 
-                <!-- <button
+                <button
                     editar="<?=$d->codigo?>"
                     class="btn btn-success btn-xs"
                     data-bs-toggle="offcanvas"
@@ -77,15 +119,15 @@
                 >
                     Ed
                 </button>
-                <button excluir="<?=$codigo?>" class="btn btn-danger btn-xs">ex</button> -->
+                <button excluir="<?=$codigo?>" class="btn btn-danger btn-xs">ex</button>
             </td>
-        </tr>
+        </tr> -->
         <?php
         }
         ?>
-    </tbody>
-</table>
-
+    <!-- </tbody>
+</table> -->
+</div>
 <script>
     $(document).ready(function () {
         Carregando('none');
