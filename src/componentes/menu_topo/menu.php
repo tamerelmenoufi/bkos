06feb54f0f3dbfd4f7a2375a1abc9c33c2e3d9cd
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-18 12:03:17 -0400

    Atualizações

--- src/componentes/menu_topo/menu.php
+++ src/componentes/menu_topo/menu.php
@@ -12,7 +12,13 @@
   <div class="container-fluid">
     <div data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
       <img src="img/logo_h60_fundo_escuro.png" style="height:40px; margin-right:20px;" alt="Sistema de Gestão QrAtivos">
+      <?php
+      if($_SESSION['QrAtivosPerfil'] == 'adm'){
+      ?>
       <i class="fa-solid fa-bars"></i>
+      <?php
+      }
+      ?>
     </div>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
