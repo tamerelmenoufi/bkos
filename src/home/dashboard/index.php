3dc3b6e49ae1749be13e4c696acaada0837b8e52
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-18 15:46:23 -0400

    Atualizações

--- src/home/dashboard/index.php
+++ src/home/dashboard/index.php
@@ -2,11 +2,61 @@
     include("{$_SERVER['DOCUMENT_ROOT']}/bkos/lib/includes.php");
 ?>
 <div class="col">
-  <div class="m-3">
 
-    <h4>Título da página</h4>
+  <div class="row">
+    <div class="m-3">
+      <h4>Título da página</h4>
+      <div class="row">
 
+        <div class="col-md-2">
+          <button class="btn btn-danger btn-block">
+            <h2>600</h2>
+            Empresas
+          </button>
+        </div>
+
+        <div class="col-md-2">
+          <button class="btn btn-danger btn-block">
+            <h2>600</h2>
+            Empresas
+          </button>
+        </div>
+
+        <div class="col-md-2">
+          <button class="btn btn-danger btn-block">
+            <h2>600</h2>
+            Empresas
+          </button>
+        </div>
+
+        <div class="col-md-2">
+          <button class="btn btn-danger btn-block">
+            <h2>600</h2>
+            Empresas
+          </button>
+        </div>
+
+        <div class="col-md-2">
+          <button class="btn btn-danger btn-block">
+            <h2>600</h2>
+            Empresas
+          </button>
+        </div>
+
+        <div class="col-md-2">
+          <button class="btn btn-danger btn-block">
+            <h2>600</h2>
+            Empresas
+          </button>
+        </div>
+
+      </div>
+    </div>
   </div>
+
+
+
+
 </div>
 
 <script>
