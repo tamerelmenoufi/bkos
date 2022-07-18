3db1c8b879f91a291fce4c769f432cd4f9e0ffda
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-18 09:12:17 -0400

    Atualizações

--- src/componentes/menu_lateral_direita/menu.php
+++ src/componentes/menu_lateral_direita/menu.php
@@ -1,13 +1,14 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/bkos/lib/includes.php");
 ?>
+<!-- style="--bs-offcanvas-width:60%;" -->
 <div
     class="offcanvas offcanvas-end"
     data-bs-backdrop="static"
     tabindex="-1"
     id="offcanvasDireita"
     aria-labelledby="offcanvasDireitaLabel"
-    style="--bs-offcanvas-width:60%;"
+
   >
   <div class="offcanvas-header">
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
