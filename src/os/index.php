98ce9a342bc6661a5bb28668a0edefafe8fa5878
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-19 16:40:54 -0400

    Atualizações

--- src/os/index.php
+++ src/os/index.php
@@ -33,7 +33,55 @@
     </div>
 </div>
 
+
+
 <div class="col">
+
+<div class="d-none d-md-block">
+    <div class="row p-2 tb-b">
+        <h5 class="col-md-2">O.S.</h5>
+        <h5 class="col-md-2">Título</h5>
+        <h5 class="col-md-2">Data</h5>
+        <h5 class="col-md-2">Situação</h5>
+        <h5 class="col-md-2">Ações</h5>
+    </div>
+</div>
+
+<?php
+while($d = mysqli_fetch_object($result)){
+?>
+
+<div class="row p-2 tb-b">
+    <div class="col-md-2">#<?=str_pad($d->codigo , 6 , '0' , STR_PAD_LEFT)?></div>
+    <div class="col-md-2"><?=$d->titulo?></div>
+    <div class="col-md-2"><?=$d->data_cadastro?></div>
+    <div class="col-md-2"><?=$d->situacao?></div>
+    <div class="col-md-2">
+
+
+        <div class="dropdown">
+            <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="acoesOs" data-bs-toggle="dropdown" aria-expanded="false">
+                Ações
+            </button>
+            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="acoesOs">
+                <li os="<?=$d->codigo?>" url="src/os/servicos_form.php"><a class="dropdown-item" href="#">Editar</a></li>
+                <li os='<?=$d->codigo?>' url="src/os/fotos.php"><a class="dropdown-item" href="#">Registro Fotográfico</a></li>
+                <li os='<?=$d->codigo?>' url="src/os/eventos.php"><a class="dropdown-item" href="#">Registro de Eventos</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="src/os/relatorio/print.php?os=<?=$d->codigo?>" target="_blank">Relatório</a></li>
+            </ul>
+        </div>
+
+    </div>
+</div>
+<?php
+}
+?>
+</div>
+
+
+
+<!-- <div class="col">
 
     <div class="d-none d-md-block">
         <div class="row p-2 tb-b">
@@ -66,9 +114,6 @@
                 <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="acoesOs">
                     <li linha='<?=$d->codigo?>'><a class="dropdown-item" href="#">Editar</a></li>
                     <li servico='<?=$d->codigo?>'><a class="dropdown-item" href="#">Ordem de Serviços</a></li>
-                    <!-- <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Separated link</a></li> -->
                 </ul>
             </div>
 
@@ -79,56 +124,109 @@
         <?php
         }
         ?>
-</div>
+</div> -->
+
+
+
+
+
 <script>
     $(document).ready(function () {
+        // Carregando('none');
+        // $("button[offcanvasDireita]").click(function(){
+        //     Carregando();
+        //     $.ajax({
+        //         url:"src/os/form.php",
+        //         success:function(dados){
+        //             $(".LateralDireita").html(dados);
+        //         }
+        //     });
+        // });
+
+
+        // $("li[linha]").click(function(){
+        //     Carregando();
+        //     os = $(this).attr("linha");
+        //     $.ajax({
+        //         url:"src/os/form.php",
+        //         type:"POST",
+        //         data:{
+        //             os,
+        //         },
+        //         success:function(dados){
+        //             $(".LateralDireita").html(dados);
+        //             let myOffCanvas = document.getElementById('offcanvasDireita');
+        //             let openedCanvas = bootstrap.Offcanvas.getInstance(myOffCanvas);
+        //             openedCanvas.show();
+        //         }
+        //     });
+        // });
+
+
+
+        // $("li[servico]").click(function(){
+        //     Carregando();
+        //     servico = $(this).attr("servico");
+        //     $.ajax({
+        //         url:"src/os/servicos.php",
+        //         type:"POST",
+        //         data:{
+        //             servico,
+        //         },
+        //         success:function(dados){
+        //             // $(".LateralDireita").html(dados);
+        //             $(".tab-pane").html(dados);
+        //         }
+        //     });
+        // });
+
+
         Carregando('none');
-        $("button[offcanvasDireita]").click(function(){
+
+        $("button[voltar]").click(function(){
             Carregando();
             $.ajax({
-                url:"src/os/form.php",
-                success:function(dados){
-                    $(".LateralDireita").html(dados);
-                }
-            });
-        });
-
-
-        $("li[linha]").click(function(){
-            Carregando();
-            os = $(this).attr("linha");
-            $.ajax({
-                url:"src/os/form.php",
-                type:"POST",
-                data:{
-                    os,
-                },
-                success:function(dados){
-                    $(".LateralDireita").html(dados);
-                    let myOffCanvas = document.getElementById('offcanvasDireita');
-                    let openedCanvas = bootstrap.Offcanvas.getInstance(myOffCanvas);
-                    openedCanvas.show();
-                }
-            });
-        });
-
-
-
-        $("li[servico]").click(function(){
-            Carregando();
-            servico = $(this).attr("servico");
-            $.ajax({
-                url:"src/os/servicos.php",
-                type:"POST",
-                data:{
-                    servico,
-                },
+                url:"src/os/index.php",
                 success:function(dados){
                     // $(".LateralDireita").html(dados);
                     $(".tab-pane").html(dados);
                 }
             });
         });
+
+        $("button[offcanvasDireita]").click(function(){
+            Carregando();
+            $.ajax({
+                url:"src/os/servicos_form.php",
+                success:function(dados){
+                    $(".LateralDireita").html(dados);
+                }
+            });
+        });
+
+
+        $("li[os]").click(function(){
+            os = $(this).attr("os");
+            url = $(this).attr("url");
+            Carregando();
+            $.ajax({
+                url,
+                type:"POST",
+                data:{
+                    os,
+                },
+                success:function(dados){
+                    $(".LateralDireita").html(dados);
+
+                    let myOffCanvas = document.getElementById('offcanvasDireita');
+                    let openedCanvas = new bootstrap.Offcanvas(myOffCanvas);
+                    // let openedCanvas = bootstrap.Offcanvas.getInstance(myOffCanvas);
+                    openedCanvas.show();
+
+                }
+            });
+        });
+
 
 
     });
