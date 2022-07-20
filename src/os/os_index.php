340b9bcfc996cfadb22abe376750d5e8e80d4fd5
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-19 21:42:56 -0400

    Atualizações

--- src/os/os_index.php
+++ src/os/os_index.php
@@ -1,17 +1,19 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/bkos/lib/includes.php");
 ?>
-<h5>Lista das Empresas</h5>
-<?php
-    $query = "select * from empresas where situacao = '1' order by razao_soial";
-    $result = mysqli_query($con, $query);
-    while($d = mysqli_fetch_object($result)){
-?>
-<div class="col-3">
-    <button class="btn btn-primary btn-block btn-lg">
-        <?=$d->razao_social?>
-    </button>
+<div class="p-3">
+    <h5>Lista das Empresas</h5>
+    <?php
+        $query = "select * from empresas where situacao = '1' order by razao_social";
+        $result = mysqli_query($con, $query);
+        while($d = mysqli_fetch_object($result)){
+    ?>
+    <div class="col-3">
+        <button class="btn btn-primary btn-block btn-lg">
+            <?=$d->razao_social?>
+        </button>
+    </div>
+    <?php
+        }
+    ?>
 </div>
-<?php
-    }
-?>
