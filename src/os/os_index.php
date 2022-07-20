3534871a01dd18855227058432f6f4767eecc508
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-19 21:46:57 -0400

    Atualizações

--- src/os/os_index.php
+++ src/os/os_index.php
@@ -8,7 +8,7 @@
         $result = mysqli_query($con, $query);
         while($d = mysqli_fetch_object($result)){
     ?>
-    <div class="col-3">
+    <div class="col-3 mt-3">
         <button class="btn btn-primary btn-block btn-lg">
             <h2><i class="fa-solid fa-building"></i></h2>
             <?=$d->razao_social?>
