07d345411fcdfba0bb2be39fc3723ecbfb5ff2e3
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-19 21:41:41 -0400

    Atualizações

--- src/home/os.php
+++ src/home/os.php
@@ -23,7 +23,7 @@
             ['src/componentes/menu_topo/menu.php','paginaHomeTopo'],
             // ['src/componentes/menu_lateral/menu.php','paginaHomeLateral'],
             ['src/componentes/menu_lateral_direita/menu.php','paginaHomeLateralDireita'],
-            ['src/os/os_lista.php','paginaHome'],
+            ['src/os/os_index.php','paginaHome'],
             ];
 
         for(i=0;i<pags.length;i++){
