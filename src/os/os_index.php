d3ea6ece62ce820840e21c39937cfe89286f73ed
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-19 22:52:24 -0400

    Atualizações

--- src/os/os_index.php
+++ src/os/os_index.php
@@ -9,7 +9,6 @@
             $query = "select * from empresas where situacao = '1' order by razao_social";
             $result = mysqli_query($con, $query);
             while($d = mysqli_fetch_object($result)){
-                for($i=0;$i<10;$i++){
         ?>
         <div class="col-3 mt-3">
             <button empresa="<?=$d->codigo?>" class="btn btn-primary btn-block btn-lg">
@@ -18,7 +17,6 @@
             </button>
         </div>
         <?php
-                }
             }
         ?>
     </div>
