38dfb7dc2170591aad3d7975e5be0f868e926564
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-25 11:14:27 -0400

    Atualizações

--- src/os/fotos.php
+++ src/os/fotos.php
@@ -88,6 +88,14 @@
         top:8px;
         z-index:0;
     }
+    .Rodape<?=$md5?> {
+        position:absolute;
+        left:0px;
+        bottom:0px;
+        right:20px;
+        z-index:0;
+        background-color:#eee;
+    }
     .Foto{
         position:relative;
         width:100%;
@@ -209,18 +217,21 @@
     </div>
     <div class="row">
         <div class="col" >
-            <div style="position:fixed; bottom:0; right:20px; z-index:10; padding:10px;">
-                <div style="display:flex; justify-content:end" >
 
-                    <div class="mb-3 form-check" style="margin-top:10px; margin-right:70px;">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">OS. Concluída?</label>
-                    </div>
+        </div>
+    </div>
 
-                    <button type="submit" SalvarFoto class="btn btn-success btn-ms">Salvar</button>
-                    <input type="hidden" id="cod_os" value="<?=$_POST['os']?>" />
-                </div>
+
+    <div class="Rodape<?=$md5?>">
+        <div style="display:flex; justify-content:end" >
+
+            <div class="mb-3 form-check" style="margin-top:10px; margin-right:70px;">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">OS. Concluída?</label>
             </div>
+
+            <button type="submit" SalvarFoto class="btn btn-success btn-ms">Salvar</button>
+            <input type="hidden" id="cod_os" value="<?=$_POST['os']?>" />
         </div>
     </div>
 <!-- </form> -->
