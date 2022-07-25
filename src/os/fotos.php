5a847b39308e6ee29e6dce186c09ad4be0d56e28
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-25 11:03:09 -0400

    Atualizações

--- src/os/fotos.php
+++ src/os/fotos.php
@@ -209,7 +209,7 @@
     </div>
     <div class="row">
         <div class="col" >
-            <div style="position:fixed; bottom:0; right:20; background-color:yellow; z-index:10; padding:10px;">
+            <div style="position:fixed; bottom:0; right:20px; background-color:yellow; z-index:10; padding:10px;">
                 <div style="display:flex; justify-content:end" >
                     <button type="submit" SalvarFoto class="btn btn-success btn-ms">Salvar X</button>
                     <input type="hidden" id="cod_os" value="<?=$_POST['os']?>" />
