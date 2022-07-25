618972500f6320a1070d0f36200742de01eb6e2b
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-25 16:59:27 -0400

    Atualizações

--- src/home/dashboard/index.php
+++ src/home/dashboard/index.php
@@ -91,10 +91,10 @@
 
 <div class="row">
   <div class="col">
-    <canvas id="myChart<?= $md5 ?>" width="400" height="400"></canvas>
+    <canvas id="Registros<?= $md5 ?>" width="400" height="400"></canvas>
   </div>
   <div class="col">
-    <canvas id="myChart<?= $md5 ?>" width="400" height="400"></canvas>
+    <canvas id="Tipos<?= $md5 ?>" width="400" height="400"></canvas>
   </div>
 </div>
 
@@ -127,7 +127,7 @@
 
 ?>
 
-    const ctx<?=$md5?> = document.getElementById('myChart<?=$md5?>');
+    const ctx<?=$md5?> = document.getElementById('Registros<?=$md5?>');
     const myChart<?=$md5?> = new Chart(ctx<?=$md5?>,
         {
             type: 'bar',
@@ -166,7 +166,7 @@
       }*/,
                     title: {
                         display: true,
-                        text: 'Quantitativo O.S.'
+                        text: 'Status de O.S.'
                     },
 
 
@@ -195,6 +195,98 @@
             },
         }
     );
+
+//////////////////////////////////////////////////////////////////////////////////
+
+<?php
+
+    $query = "
+        select
+              a.titulo,
+              (select count(*) from os b where b.tipo = a.codigo) as qt
+        from os_tipos a where a.situacao = '1'
+    ";
+    $result = mysqli_query($con, $query);
+    $Rotulos = [];
+    $Quantidade = [];
+    while($d = mysqli_fetch_object($result)){
+      $Rotulos[] = $d->titulo;
+      $Quantidade[] = $d->qt;
+    }
+    $R = (($Rotulos)?"'".implode("','",$Rotulos)."'":0);
+    $Q = (($Quantidade)?implode(",",$Quantidade):0);
+
+?>
+
+    const ctx<?=$md5?> = document.getElementById('Tipos<?=$md5?>');
+    const myChart<?=$md5?> = new Chart(ctx<?=$md5?>,
+        {
+            type: 'bar',
+            data: {
+                labels: [<?=$R?>],
+                datasets: [{
+                    label: [<?=$R?>],
+                    data: [<?=$Q?>],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                    ],
+                    borderWidth: 1,
+                    rotulos: [<?=$R?>]
+                }]
+            },
+            options: {
+                indexAxis: 'y',
+                // Elements options apply to all of the options unless overridden in a dataset
+                // In this case, we are setting the border of each horizontal bar to be 2px wide
+                elements: {
+                    bar: {
+                        borderWidth: 2,
+                    }
+                },
+                responsive: true,
+                plugins: {
+                    legend: false/*{
+        position: 'right',
+      }*/,
+                    title: {
+                        display: true,
+                        text: 'Tipos de O.S.'
+                    },
+
+
+                    tooltip: {
+                        callbacks: {
+                            title: function (context) {
+                                indx = context[0].parsed.y;
+                                return context[0].dataset.rotulos[indx];
+                            },
+                            label: function (context) {
+                                indx = context.parsed.y;
+                                var label = ' ' + context.dataset.label[indx] || '';
+
+                                if (label) {
+                                    label += ' : ';
+                                }
+                                if (context.parsed.y !== null) {
+                                    label += context.parsed.x + ' Registro(s)';
+                                }
+                                return label;
+                            }
+                        }
+                    }
+
+                }
+            },
+        }
+    );
+
 
 
 </script>
