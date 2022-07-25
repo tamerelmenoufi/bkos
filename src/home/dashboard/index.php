c1f71859550cd655ced0c513199bf1afdfd0effb
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-25 16:25:05 -0400

    Atualizações

--- src/home/dashboard/index.php
+++ src/home/dashboard/index.php
@@ -89,8 +89,89 @@
 
 </div>
 
+<div class="row">
+  <div class="col">
+    <canvas id="myChart<?= $md5 ?>" width="400" height="400"></canvas>
+  </div>
+</div>
+
+
 <script>
   $(function(){
     Carregando('none');
   })
+
+
+
+
+    const ctx<?=$md5?> = document.getElementById('myChart<?=$md5?>');
+    const myChart<?=$md5?> = new Chart(ctx<?=$md5?>,
+        {
+            type: 'bar',
+            data: {
+                labels: ['A','B','C'],
+                datasets: [{
+                    label: ['A','B','C'],
+                    data: ['12','18','15'],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                    ],
+                    borderWidth: 1,
+                    rotulos: ['A','B','C']
+                }]
+            },
+            options: {
+                indexAxis: 'y',
+                // Elements options apply to all of the options unless overridden in a dataset
+                // In this case, we are setting the border of each horizontal bar to be 2px wide
+                elements: {
+                    bar: {
+                        borderWidth: 2,
+                    }
+                },
+                responsive: true,
+                plugins: {
+                    legend: false/*{
+        position: 'right',
+      }*/,
+                    title: {
+                        display: true,
+                        text: 'Quantitativo O.S.'
+                    },
+
+
+                    tooltip: {
+                        callbacks: {
+                            title: function (context) {
+                                indx = context[0].parsed.y;
+                                return context[0].dataset.rotulos[indx];
+                            },
+                            label: function (context) {
+                                indx = context.parsed.y;
+                                var label = ' ' + context.dataset.label[indx] || '';
+
+                                if (label) {
+                                    label += ' : ';
+                                }
+                                if (context.parsed.y !== null) {
+                                    label += context.parsed.x + ' Registro(s)';
+                                }
+                                return label;
+                            }
+                        }
+                    }
+
+                }
+            },
+        }
+    );
+
+
 </script>
