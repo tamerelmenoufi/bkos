2a07ab8c5406ec510fc92024d30dca128742b89f
tamerelmenoufi <tamer@mohatron.com.br>
2022-08-30 21:40:52 -0400

    Atualizações

--- lib/includes.php
+++ lib/includes.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
     include("/appinc/connect.php");
-    include("./wapp/send.php");
+    include("./vendor/wapp/send.php");
     $con = AppConnect('bkos');
     $md5 = md5(date("YmdHis"));
