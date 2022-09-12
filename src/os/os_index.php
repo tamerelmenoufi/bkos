7f12b468112bea49f8235e66182e97fadb6592eb
tamerelmenoufi <tamer@mohatron.com.br>
2022-09-11 22:21:33 -0400

    Atualizações

--- src/os/os_index.php
+++ src/os/os_index.php
@@ -10,7 +10,7 @@
             $result = mysqli_query($con, $query);
             while($d = mysqli_fetch_object($result)){
         ?>
-        <div class="col-3 mt-3">
+        <div class="col-md-3 mt-3">
             <button empresa="<?=$d->codigo?>" class="btn btn-primary btn-block btn-lg">
                 <h2><i class="fa-solid fa-building"></i></h2>
                 <?=$d->razao_social?>
