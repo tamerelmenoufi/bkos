9a98c11d15cf156c0fbd3196a6ee9d59a501b5ce
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-03 11:18:44 -0400

    Atualizações

--- src/home/dashboard/index.php
+++ src/home/dashboard/index.php
@@ -169,7 +169,19 @@
               </li>
             </ul>
             <div class="tab-content" id="myTabContent">
-              <div class="tab-pane fade show active moldura" id="painelCritico" role="tabpanel" aria-labelledby="home-tab" tabindex="0">...</div>
+              <div
+                  class="tab-pane fade show active"
+                  id="painelCritico"
+                  role="tabpanel"
+                  aria-labelledby="home-tab"
+                  tabindex="0"
+                  style="
+                          border-left:1px solid #ccc;
+                          border-right:1px solid #ccc;
+                          border-bottom:1px solid #ccc;
+                          padding:10px;
+                        "
+              >...</div>
               <!-- <div class="tab-pane fade" id="profile-tab-pane" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">...</div>
               <div class="tab-pane fade" id="contact-tab-pane" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">...</div>
               <div class="tab-pane fade" id="disabled-tab-pane" role="tabpanel" aria-labelledby="disabled-tab" tabindex="0">...</div> -->
