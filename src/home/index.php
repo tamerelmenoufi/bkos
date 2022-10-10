a120f8a653c68f6de60f864c6b0935c9fa49bd65
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-10 09:16:54 -0400

    Atualizações

--- src/home/index.php
+++ src/home/index.php
@@ -21,8 +21,8 @@
     $(function(){
         pags = [
             ['src/componentes/menu_topo/menu.php','paginaHomeTopo'],
-            ['src/componentes/menu_lateral/menu.php','paginaHomeLateral'],
-            // ['src/componentes/menu_lateral_direita/menu.php','paginaHomeLateralDireita'],
+            // ['src/componentes/menu_lateral/menu.php','paginaHomeLateral'],
+            ['src/componentes/menu_lateral_direita/menu.php','paginaHomeLateralDireita'],
             ['src/home/dashboard/index.php','paginaHome'],
             ];
 
