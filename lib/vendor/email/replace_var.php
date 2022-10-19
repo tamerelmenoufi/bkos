80a3d2ddcdb7fc0c897bab64a6c7fe343fee6afd
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-19 14:44:19 -0400

    Atualizações

---
+++ lib/vendor/email/replace_var.php
@@ -0,0 +1,104 @@
+<?php
+    include("{$_SERVER['DOCUMENT_ROOT']}/bkos/lib/includes.php");
+
+    function ReplaceVar($html){
+
+        global $con;
+
+        $Str = [];
+
+        $query = "select a.*,
+                        b.nome as executor,
+                        c.nome as responsavel,
+                        d.titulo as tipo
+                    from os a
+                    left join colaboradores b on a.executor = b.codigo
+                    left join colaboradores c on a.responsavel = c.codigo
+                    left join os_tipos d on a.tipo = d.codigo
+                    where a.codigo = '{$_GET['os']}'";
+        $result = mysqli_query($con, $query);
+        $d = mysqli_fetch_object($result);
+
+    //////////////////////////////////////////////////////////////////////////////
+
+        $Str['os']['codigo'] = str_pad($d->codigo , 6 , '0' , STR_PAD_LEFT);
+        $Str['os']['executor'] = $d->executor;
+        $Str['os']['data_cadastro'] = $d->data_cadastro;
+        $Str['os']['responsavel'] = $d->responsavel;
+        $Str['os']['titulo'] = $d->titulo;
+        $Str['os']['descricao'] = $d->descricao;
+        $Str['os']['tipo'] = $d->tipo;
+
+
+        $q = "select a.*, b.nome as colaborador from os_fotos a left join colaboradores b on a.colaborador = b.codigo where a.cod_os = '{$d->codigo}' and a.situacao = '1' and JSON_EXTRACT(a.deletado,\"$.usuario\") = ''";
+        $r = mysqli_query($con, $q);
+        $i=0;
+        while($e = mysqli_fetch_object($r)){
+            //////////////////////////////////////////////////////////////////////////////
+            $Str['os_fotos'][]['foto'] = 'http://os.bkmanaus.com.br/src/os/fotos/'.$d->codigo.'/'.$e->foto;
+            $Str['os_fotos'][]['titulo'] = $e->titulo;
+            $Str['os_fotos'][]['descricao'] = $e->descricao;
+            $Str['os_fotos'][]['colaborador'] = $e->colaborador;
+            $Str['os_fotos'][]['data_cadastro'] = $e->data_cadastro;
+            //////////////////////////////////////////////////////////////////////////////
+        }
+
+
+
+        $q = "select
+                    a.*,
+                    b.titulo as status,
+                    c.titulo as classificacao,
+                    d.nome as colaborador
+                from os_registros a
+                    left join os_status b on a.status = b.codigo
+                    left join os_classificacao c on a.classificacao = c.codigo
+                    left join colaboradores d on a.colaborador = d.codigo
+
+                where a.cod_os = '{$d->codigo}' and a.situacao = '1' and JSON_EXTRACT(a.deletado,\"$.usuario\") = '' order by a.data_cadastro asc";
+        $r = mysqli_query($con, $q);
+        $i=0;
+        while($e = mysqli_fetch_object($r)){
+            //////////////////////////////////////////////////////////////////////////////
+            $Str['os_registros'][]['classificacao'] = $e->classificacao;
+            $Str['os_registros'][]['status'] = $e->status;
+            $Str['os_registros'][]['descricao'] = $e->descricao;
+            $Str['os_registros'][]['colaborador'] = $e->colaborador;
+            $Str['os_registros'][]['data_cadastro'] = $e->data_cadastro;
+            //////////////////////////////////////////////////////////////////////////////
+        }
+
+        $Str['os']['codigo'] = str_pad($d->codigo , 6 , '0' , STR_PAD_LEFT);
+        $Str['os']['executor'] = $d->executor;
+        $Str['os']['data_cadastro'] = $d->data_cadastro;
+        $Str['os']['responsavel'] = $d->responsavel;
+        $Str['os']['titulo'] = $d->titulo;
+        $Str['os']['descricao'] = $d->descricao;
+        $Str['os']['tipo'] = $d->tipo;
+
+        foreach($Str['os'] as $i => $v){
+            $html = str_replace("{{os-{$i}}}", $v, $html);
+        }
+
+        $fotos = substr($html, stripos($html, '{{os-fotos}}'), strripos($html, '{{os-fotos}}'));
+        $html = str_replace($fotos,'{{os-fotos}}',$html);
+        $fotos = str_replace('{{os-fotos}}', false, $fotos);
+
+        $AddFotos = [];
+        foreach($Str['os-fotos'] as $i => $v){
+            $AddFotos[$i] .= str_replace("{{os-fotos-foto}}", $v['foto'], $fotos);
+            $AddFotos[$i] .= str_replace("{{os-fotos-titulo}}", $v['titulo'], $fotos);
+            $AddFotos[$i] .= str_replace("{{os-fotos-descricao}}", $v['descricao'], $fotos);
+            $AddFotos[$i] .= str_replace("{{os-fotos-colaborador}}", $v['colaborador'], $fotos);
+            $AddFotos[$i] .= str_replace("{{os-fotos-data_cadastro}}", $v['data_cadastro'], $fotos);
+            $AddFotos[$i] .= str_replace("{{os-fotos-titulo}}", $v['titulo'], $fotos);
+        }
+
+        if($AddFotos){
+            $html = str_replace('{{os-fotos}}',implode("\n",$AddFotos),$html);
+        }
+
+
+
+        return $html;
+    }
--- lib/vendor/email/tamplates/os_update.php
+++ lib/vendor/email/tamplates/os_update.php
@@ -7,6 +7,7 @@
             width:100%;
             padding:0px;
             margin:0;
+            margin-top:5px;
             font-family:'verdana';
             font-size:12px;
             color:#333;
@@ -22,50 +23,107 @@
             padding:0px;
             margin:0;
         }
+
+        .item_foto{
+            width:100%;
+            padding:0px;
+            margin:0;
+            margin-top:5px;
+            font-family:'verdana';
+            font-size:12px;
+            color:#333;
+        }
+        .item_foto p{
+            width:100%;
+            padding:0px;
+            margin:0;
+            text-align:center;
+        }
     </style>
 </head>
 <body>
+
+    <?php
+    /*
+    ?>
+    $Str['os']['codigo'] = str_pad($d->codigo , 6 , '0' , STR_PAD_LEFT);
+    $Str['os']['executor'] = $d->executor;
+    $Str['os']['data_cadastro'] = $d->data_cadastro;
+    $Str['os']['responsavel'] = $d->responsavel;
+    $Str['os']['titulo'] = $d->titulo;
+    $Str['os']['descricao'] = $d->descricao;
+    $Str['os']['tipo'] = $d->tipo;
+
+    $Str['os_fotos']['foto'][] = 'http://os.bkmanaus.com.br/src/os/fotos/'.$d->codigo.'/'.$e->foto;
+    $Str['os_fotos']['titulo'][] = $e->titulo;
+    $Str['os_fotos']['descricao'][] = $e->descricao;
+    $Str['os_fotos']['colaborador'][] = $e->colaborador;
+    $Str['os_fotos']['data_cadastro'][] = $e->data_cadastro;
+
+    $Str['os_registros']['classificacao'][] = $e->classificacao;
+    $Str['os_registros']['status'][] = $e->status;
+    $Str['os_registros']['descricao'][] = $e->descricao;
+    $Str['os_registros']['colaborador'][] = $e->colaborador;
+    $Str['os_registros']['data_cadastro'][] = $e->data_cadastro;
+    <?php
+    //*/
+    ?>
+
     <table cellspacing="0" cellpadding="0" style="border:1px #ccc solid; width:600px;">
         <tr>
             <td style="width:120px">
                 <img src="cid:logo.png" style="width:120px" >
             </td>
             <td style="padding:10px;">
-                <h2>Atualização da O.S. #{{numero_os}}</h2>
+                <h2>Atualização da O.S. #{{os-codigo}}</h2>
             </td>
         </tr>
 
         <tr>
             <td colspan="2" style="width:600px; padding:20px;">
                 <p>
-                    Uma nova atualização foi realizada em {{data_atual}}, com as seguintes informações:
+                    Uma nova atualização foi realizada em {{os-data_atual}}, com as seguintes informações:
                 </p>
                 <div class="item">
                     <span>Tipo da Solicitação</span>
-                    <p>{{tipo}}</p>
+                    <p>{{os-tipo}}</p>
                 </div>
                 <div class="item">
                     <span>Título</span>
-                    <p>{{titulo}}</p>
+                    <p>{{os-titulo}}</p>
                 </div>
                 <div class="item">
                     <span>Descrição</span>
-                    <p>{{descricao}}</p>
+                    <p>{{os-descricao}}</p>
                 </div>
                 <div class="item">
                     <span>Empresa</span>
-                    <p>{{empresa}}</p>
+                    <p>{{os-empresa}}</p>
                 </div>
                 <div class="item">
                     <span>Responsável</span>
-                    <p>{{responsavel}}</p>
+                    <p>{{os-responsavel}}</p>
                 </div>
                 <div class="item">
                     <span>Executor</span>
-                    <p>{{executor}}</p>
+                    <p>{{os-executor}}</p>
                 </div>
             </td>
         </tr>
+
+        {{os_fotos}}
+        <tr>
+            <td colspan="2" style="width:600px; padding:20px;">
+                <div class="item_foto">
+                    <img src="{{os_fotos-foto}}"  />
+                    <p>{{os_fotos-titulo}}</p>
+                    <p>{{os_fotos-descricao}}</p>
+                    <p>{{os_fotos-colaborador}}</p>
+                    <p>{{os_fotos-data_cadastro}}</p>
+                </div>
+            </td>
+        </tr>
+        {{os_fotos}}
 
     </table>
 </body>
