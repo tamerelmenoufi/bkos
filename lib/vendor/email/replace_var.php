addf01eee20ca889b467fb1429ed3f9580eff41f
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-19 15:45:40 -0400

    Atualizações

--- lib/vendor/email/replace_var.php
+++ lib/vendor/email/replace_var.php
@@ -91,7 +91,7 @@
             $html = str_replace('{{os_fotos}}',implode("",$AddF),$html);
         }
 
-        // file_put_contents('./tamplates/modelo.txt', $html);
+        file_put_contents("{$_SERVER['DOCUMENT_ROOT']}/bkos/lib/vendor/email/tamplates/modelo.txt", $html);
 
         return $html;
     }
