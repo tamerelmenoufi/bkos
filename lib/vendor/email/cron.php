b2e05d4ff2372a989d1d1a4a7a237fc8755900d9
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-25 22:05:41 -0400

    Atualizações

--- lib/vendor/email/cron.php
+++ lib/vendor/email/cron.php
@@ -1,7 +1,19 @@
 <?php
 
-    $dados = file("tamplates/resumo.php");
+    $postdata = http_build_query(
+        array(
+            'acao' => 'resumo', // Receivers phonei
+        )
+    );
+    $opts = array('http' =>
+        array(
+            'method' => 'POST',
+            'header' => 'Content-type: application/x-www-form-urlencoded',
+            'content' => $postdata
+        )
+    );
+    $context = stream_context_create($opts);
+    $result = file_get_contents('https://os.bkmanaus.com.br/lib/vendor/email/tamplates/resumo.php', false, $context);
 
-    foreach($dados as $i => $v){
-        echo $v;
-    }
+
+    echo $result;
