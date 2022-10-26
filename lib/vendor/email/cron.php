aaacf4c3584ac6bd4f27df0d066ba230cc7543c3
tamerelmenoufi <tamer@mohatron.com.br>
2022-10-25 23:14:36 -0400

    Atualizações

--- lib/vendor/email/cron.php
+++ lib/vendor/email/cron.php
@@ -26,7 +26,7 @@
     $to = ['to_name' => 'Tamer Mohamed', 'to_email' => 'tamer.menoufi@gmail.com'];
 
     $contatos = [
-        'to' => $to,
+        'to' => $to
     ];
 
     $_SESSION['MailFotosInline'][] = 'https://os.bkmanaus.com.br/img/logo.png';
