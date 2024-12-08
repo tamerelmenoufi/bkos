8368cec9473b6e8ce36e25d8e8e08efedb6b3aae
tamerelmenoufi <tamer@mohatron.com.br>
2024-12-07 22:45:50 -0400

    Atualizaçeõs

--- src/relatorios/index.php
+++ src/relatorios/index.php
@@ -3,7 +3,13 @@
 ?>
 <div class="m-3">
     <h1>Relatórios</h1>
-    <p>Em Desenvolvimento...</p>
+    <div class="input-group mb-3">
+        <label class="input-group-text" for="inputGroupSelect01">Buscar entre</label>
+        <input type="text" class="form-control" aria-label="Text input with dropdown button">
+        <label class="input-group-text" for="inputGroupSelect01">e</label>
+        <input type="text" class="form-control" aria-label="Text input with dropdown button">
+        <button type="button" class="btn btn-outline-secondary">Buscar</button>
+    </div>
 </div>
 
 <script>
