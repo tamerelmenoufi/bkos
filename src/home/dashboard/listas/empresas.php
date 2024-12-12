4a5585653e0afe14d2583d6a672e067b1dc1a4ef
Tamer Elmenoufi <tamer.menoufi@gmail.com>
2024-12-12 10:13:23 +0200

    Atualizações

--- src/home/dashboard/listas/empresas.php
+++ src/home/dashboard/listas/empresas.php
@@ -1,3 +1,34 @@
 <?php
     include("{$_SERVER['DOCUMENT_ROOT']}/bkos/lib/includes.php");
     // echo "Aqui é conteúde de ".$_POST['opc'];
+
+    $query = "select * from empresas order by razao_social asc";
+    $result = mysqli_query($con, $query);
+?>
+<div class="m-3">
+    <h2>Empresas Cadastradas</h2>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>CNPJ</th>
+                <th>Razão Social</th>
+                <th>Data de Cadastro</th>
+            </tr>
+        </thead>
+        <tbody>
+
+
+<?php
+    while($d = mysqli_fetch_object($result)){
+?>
+            <tr>
+                <td><?=$d->cnpj?></td>
+                <td><?=$d->razao_social?></td>
+                <td><?=dataBr($d->data_cadastro)?></td>
+            </tr>
+<?php
+    }
+?>
+        </tbody>
+    </table>
+</div>
